ca815b920dbdfee85045c3a5a387a27a33c068ff
Henrique Zanetti <hzanettik12@gmail.com>
2022-05-29 16:56:27 -0300

    triangle class addition - CalculoTrianglesAreas

--- CalculoTrianglesAreas/src/application/Main.java
+++ CalculoTrianglesAreas/src/application/Main.java
@@ -1,40 +1,42 @@
 package application;
 
+import entities.Triangle;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        Triangle x, y;
+        x = new Triangle();
+        y = new Triangle();
+
         System.out.println("Enter with information about triangles.");
-        System.out.println("Triangle alfa:");
-        double sideAlfaA = sc.nextDouble();
-        double sideAlfaB = sc.nextDouble();
-        double sideAlfaC = sc.nextDouble();
-        double areaAlfa = calculateArea(sideAlfaA, sideAlfaB, sideAlfaC);
-        System.out.println("area alfa: " + String.format("%.2f", areaAlfa));
 
-        System.out.println("Triangle beta:");
-        double sideBetaA = sc.nextDouble();
-        double sideBetaB = sc.nextDouble();
-        double sideBetaC = sc.nextDouble();
-        double areaBeta = calculateArea(sideBetaA, sideBetaB, sideBetaC);
-        System.out.println("Area beta: " + String.format("%.2f", areaBeta));
+        System.out.println("Triangle X:");
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
+        double areaX = x.calculateArea();
+        System.out.println("Area x: " + String.format("%.2f", areaX));
 
-        if( areaAlfa > areaBeta){
-            System.out.printf("The triangle with the largest area is alfa. Area: %.2f%n", areaAlfa);
+        System.out.println("Triangle Y:");
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
+        double areaY = y.calculateArea();
+        System.out.println("Area x: " + String.format("%.2f", areaY));
+
+        if( areaX > areaY){
+            System.out.printf("The triangle with the largest area is X. Area: %.2f%n", areaX);
         } else {
-            System.out.printf("The triangle with the largest area is beta. Area: %.2f", areaBeta);
+            System.out.printf("The triangle with the largest area is Y. Area: %.2f", areaY);
         }
+
 
 
         sc.close();
     }
 
-    public static double calculateArea(double a, double b, double c){
-        double p, area;
-        p = (a + b + c) / 2;
-        area = Math.sqrt(p*(p-a)*(p-b)*(p-c));
-        return area;
-    }
 }
