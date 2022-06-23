2d738d104c66195e9d0547cea8aae30ce8eb112b
Henrique Zanetti <hzanettik12@gmail.com>
2022-06-22 22:05:25 -0300

    Change project shape

--- Shape/src/application/Main.java
+++ Shape/src/application/Main.java
@@ -7,21 +7,39 @@ import entities.Shape;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
         List<Shape> list = new ArrayList<>();
 
-        Shape s1 = new Circle(Color.valueOf("Black"), 3.21);
-        Shape s2 = new Circle(Color.valueOf("Green"), 3.0);
-        Shape s3 = new Rectangle(Color.valueOf("White"), 4.0, 5.0);
-        Shape s4 = new Rectangle(Color.valueOf("Red"), 1.12, 5.4);
-        list.add(s1);
-        list.add(s2);
-        list.add(s3);
-        list.add(s4);
+        System.out.println("Inform the number of shapes: ");
+        int n = sc.nextInt();
 
+        for(int i=0; i<n; i++){
+            System.out.println(" === Info about shape " + (i+1) + " ===");
+            System.out.print("Color: (Black,White, Red, Blue, Green): ");
+            Color color = Color.valueOf(sc.next());
+            System.out.print("Circle or Rectangle: ");
+            char ch = sc.next().charAt(0);
+            if(ch == 'r' || ch == 'R'){
+                System.out.print("Width: ");
+                double width = sc.nextDouble();
+                System.out.print("Height: ");
+                double height = sc.nextDouble();
+                list.add(new Rectangle(color, width,height));
+            } else {
+                System.out.print("radius of the circle: ");
+                double radius = sc.nextDouble();
+                list.add(new Circle(color, radius));
+            }
+        }
+
+        System.out.println();
+        System.out.println("== Shapes ==");
         for(Shape shape: list){
             System.out.println(shape);
         }
